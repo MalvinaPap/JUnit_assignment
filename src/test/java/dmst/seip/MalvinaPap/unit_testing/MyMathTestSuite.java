9c89ba17469af63692c85d0a8a64221d7e2526c0
MalvinaPap <t8150108@dias.aueb.gr>
2018-04-03 20:42:28 +0300

    added suite class

---
+++ src/test/java/dmst/seip/MalvinaPap/unit_testing/MyMathTestSuite.java
@@ -0,0 +1,18 @@
+package dmst.seip.MalvinaPap.unit_testing;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * This class remains empty. It is
+ * used as a holder for the annotations
+ * below. Its mission is to run more than
+ * test classes at the same time
+ * @author Malvina
+ *
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MyMathTest.class,MyMathTest_ReverseNumber_Parameterized.class})
+public class MyMathTestSuite {
+
+}
