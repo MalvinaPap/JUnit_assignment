163235e7bf4a36e8c2e0999a4eba5bbfb3226a3c
MalvinaPap <t8150108@dias.aueb.gr>
2018-04-03 19:08:41 +0300

    add reverseNumber method to MyMath class

--- src/main/java/dmst/seip/MalvinaPap/unit_testing/MyMath.java
+++ src/main/java/dmst/seip/MalvinaPap/unit_testing/MyMath.java
@@ -1,27 +1,37 @@
 package dmst.seip.MalvinaPap.unit_testing;
 /**
- * An class that provides some "simple" math operations
- * for demonstrating Unit Testing.
+ * An class that provides some simple math operations
  * @author Malvina
  */
 import java.lang.IllegalArgumentException;
 
-/**
- * receives 2 integers and returns their division result
- * or throws exception if the denominator is zero
- * @param num the numerator
- * @param num the denominator
- * @throws IllegalArgumentException
- *
- */
-public class MyMath {
 
+public class MyMath {
+    
+	/**
+	 * receives 2 integers and returns their division result
+	 * or throws exception if the denominator is zero
+	 * @param num the numerator
+	 * @param num the denominator
+	 * @throws IllegalArgumentException
+	 *
+	 */
 	double divide(int num, int denom) {
 		if (denom==0) {
 			throw new IllegalArgumentException("'denominator' cannot be zero");
 		}
 		double result = num/denom;
 		return result;
+	}
+	
+
+	/**
+	 * Reverses the sign of a given number
+	 * @param number the input number 
+	 * @return a number with reversed sign
+	 */
+	public int reverseNumber(int number) {
+		return -number;
 	}
 	
 }
