93d5e3060d5309896b2e09abca27a394224adcfa
MalvinaPap <t8150108@dias.aueb.gr>
2018-04-03 18:58:52 +0300

    finished MyArrayOperationsTest

--- src/test/java/dmst/seip/MalvinaPap/unit_testing/MyArrayOperationsTest.java
+++ src/test/java/dmst/seip/MalvinaPap/unit_testing/MyArrayOperationsTest.java
@@ -1,7 +1,9 @@
 package dmst.seip.MalvinaPap.unit_testing;
 
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -15,5 +17,34 @@ import java.io.IOException;
  */
 
 public class MyArrayOperationsTest {
-
+	
+	MyArrayOperations opers = new MyArrayOperations();
+	
+	/**
+	 * This test method checks if method
+	 * gradeFrequencies works correctly. For 
+	 * this purpse method readFile gets mocked
+	 * so that it does not affect the result of
+	 * the test. 2 files are used for the test.
+	 */
+	@Test
+	public void gradefrequenciesMocking() throws IOException, FileNotFoundException {
+		// Mock the MyFileUtilities dependency
+		MyFileUtilities utils = mock(MyFileUtilities.class);
+		int[] gd1 = new int[]{4,6,7,5,7,6,9,10,10,5,6,7,7,7,8,9,5,8,9,8,8,9,6};
+		int[] gd2 = new int[]{1,2,3,4,5,6,7,8,9,10};
+		int[] gd3 = new int[]{8,8,8,8,7,7};
+		
+		// Pre-define the results of the readFile calls
+		when(utils.readFile("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades1.txt")).thenReturn(gd1);
+		when(utils.readFile("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades2.txt")).thenReturn(gd2);
+		when(utils.readFile("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades3.txt")).thenReturn(gd3);
+		
+		Assert.assertArrayEquals(new int[]{0,0,0,0,1,3,4,5,4,4,2},opers.gradeFrequencies
+				("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades1.txt",utils));
+		Assert.assertArrayEquals(new int[]{0,1,1,1,1,1,1,1,1,1,1},opers.gradeFrequencies
+				("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades2.txt",utils));
+		Assert.assertArrayEquals(new int[]{0,0,0,0,0,0,0,2,4,0,0},opers.gradeFrequencies
+				("C:\\Users\\admin\\Desktop\\JUnit_assignment\\src\\test\\resources.grades3.txt",utils));
+	}
 }
